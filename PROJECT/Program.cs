cac94097b9829ccb06368aa5b74646ae957e215c
Serge Volynets <sergej-volynec@yandex.ru>
2023-03-22 18:51:30 +0300

    ArrayInitialization

---
+++ PROJECT/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/// <summary>
+/// Создает новый массив строк, которые пользователь вводит с консоли
+/// </summary>
+/// <returns>Возвращает введенный массив строк</returns>
+string[] InitializeArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] initArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i}-ый элемент массива: ");
+        string element = Console.ReadLine();
+        initArray[i] = element.Trim();
+    }
+    return initArray;
+}
+
